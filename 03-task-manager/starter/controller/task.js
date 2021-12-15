5787f2bf7ab0c46a015db21868a90b20905ec76b
delaquash <olaide1191@gmail.com>
2021-12-15 15:15:26 +0100

    fixed getTaskByID

--- 03-task-manager/starter/controller/task.js
+++ 03-task-manager/starter/controller/task.js
@@ -1,5 +1,6 @@
 const Task = require('../model/task');
 
+// GET ALL TASK
 const getAllTask = async (req, res) => {
     try {
         const task = await Task.find({})
@@ -8,20 +9,33 @@ const getAllTask = async (req, res) => {
         res.status(500).json({ msg : error })
     }
 }
-
+// To create task
 const createTask = async (req, res) => {
-
     try {
         const task = await Task.create(req.body)
-        res.status(202).json({ task })
+        res.status(201).json({ task })
     } catch (error) {
         res.status(500).json({ msg : error })
     }
     
 }
-const getTask = (req, res) => {
+
+// to get a task by id
+const getTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id: taskID})
+        // if there is no task at all
+        if(!task){
+          return res.status(404).json({ msg: `No task with the id: ${taskID}`}) 
+        }
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg : error })
+    }
     res.json({ id: req.params.id })
 }
+
 
 const updateTask = (req, res) => {
     res.send('Update Task')
