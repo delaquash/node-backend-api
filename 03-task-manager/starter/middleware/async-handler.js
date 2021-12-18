19d630dc94830c63c55e5a4c1b0758b4ab2f469d
delaquash <olaide1191@gmail.com>
2021-12-18 19:36:30 +0100

    fixed asyncHandler for getAllTask

--- 03-task-manager/starter/middleware/async-handler.js
+++ 03-task-manager/starter/middleware/async-handler.js
@@ -1,5 +1,11 @@
-const asyncHandler = () => {
-
+const asyncHandler = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn( req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports = asyncHandler;
