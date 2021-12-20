561c61259c83e8ec7ddceb4b61404c34d8de92ff
delaquash <olaide1191@gmail.com>
2021-12-20 13:37:35 +0100

    implemented async-errors for store-api

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,6 +2,11 @@ const getAllProductsStatic = async(req, res) => {
     res.status(200).json({msg: "Products testing route"})
 }
 
+const getAllProducts= async (req, res) => {
+    res.status(200).json({ msg:"Specific Product testing route"})
+}
+
 module.exports = {
-    getAllProductsStatic
+    getAllProductsStatic,
+    getAllProducts
 }
