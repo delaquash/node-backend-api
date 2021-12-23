f5192f13109fb61edae138cd392124fe03bb494b
delaquash <olaide1191@gmail.com>
2021-12-23 22:58:55 +0100

    implemented sort for store-api

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,40 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async(req, res) => {
-    const product = await Product.find( {featured: true} )
+    // using regex to find a particular name from products.json using a value declared in search
+    // const search = "ae"
+    // const product = await Product.find({ 
+    //     name: {
+    //         $regex: search,
+    //         $options: 'i'
+    //     }
+    // })
+    const product = await Product.find({}).sort("-name price")
     res.status(200).json({ product, nbHits: product.length  })
 }
 
 const getAllProducts= async (req, res) => {
-    const product = await Product.find(req.query)
+
+    const { featured, company, name, sort } = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false
+        
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    // console.log(queryObject)
+    let result = Product.find(queryObject)
+    if(sort){
+        // product = result
+        console.log(sort)
+    }
+    const product = await result
     res.status(200).json({  product, nbHits: product.length })
 }
 
