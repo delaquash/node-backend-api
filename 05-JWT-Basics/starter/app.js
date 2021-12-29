cbe6900e54aa7677842edb68bc2602a76c4ac297
delaquash <olaide1191@gmail.com>
2021-12-29 13:53:14 +0100

    implemented route for jwt-basics

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -6,15 +6,18 @@ const app = express();
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const mainRouter = require('./routes/main');
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+app.use('/api/v1', mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,17 +1,25 @@
+// check username /password in post(login) request
+// if it exist, create jwt
+// send back to frontend
+// set up auth so only user/request with jwt can access the dashboard
 
-
+const CustomAPIError = require("../errors/custom-error");
 
 const login = async(req, res) => {
+    const { username, password } = req.body;
+    if(!username || !password){
+        throw new CustomAPIError("Please provide username and password", 400)
+    }
     res.send('Fake login/register/signup route')
 }
 
-const dashboard = async (req, res) => {
+const dashboard = async (req, res) => { 
     const luckyNumber = Math.floor(Math.random() * 100)
     res.status(200).json({msg: `Hello, Olaide Emmanuel`, secret:`Here is your authorized data,
     your lucky number is ${luckyNumber}`})
 }
 
-module.exports ={
+module.exports = {
     login,
     dashboard
 }
