c39087110650a1d0ca70eca197f7525b65879644
delaquash <olaide1191@gmail.com>
2022-01-18 18:20:38 +0100

    implemented router function on jobs-api

---
+++ 06-jobs-api/starter/controllers/auth.js
@@ -0,0 +1,12 @@
+const register = async(req, res)=> {
+    res.send("register route")
+}
+
+ const login = async (req, res) => {
+     res.send("login route")
+ }
+
+ module.exports = {
+     register, 
+     login
+ }
