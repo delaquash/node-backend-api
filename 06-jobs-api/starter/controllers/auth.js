e9b4e1bb69fa4a51496a1c5d602f25b2251d180f
delaquash <olaide1191@gmail.com>
2022-01-25 12:56:11 +0100

    implemented bcrypt/hashing functionalities on register route in userjs of job-api

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,13 +1,14 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 const { BadRequestError } = require('../errors/bad-request');
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 // post request
 const register = async(req, res)=> {
-   
     const user = await User.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json({ user })
+    // invoking the jwt function in user.js/model
+    const token = user.createJWT();
+    res.status(StatusCodes.CREATED).json({ user: {name: user.name}, token })
 }
 
  const login = async (req, res) => {
