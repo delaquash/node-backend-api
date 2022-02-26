d0a2188d2f71a1419bdbf4aa2864c92a0b61f75d
delaquash <olaide1191@gmail.com>
2022-02-26 03:04:36 +0100

    implemented create job route for createJob in job-api

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,3 +1,7 @@
+const { StatusCodes } = require("http-status-codes");
+const { NotFoundError, BadRequestError } = require("../errors");
+const Job = require("../models/Job");
+
 const getAllJobs = async(req, res)=> {
     res.send('get all the job')
 }
@@ -7,7 +11,9 @@ const getJob = async(res, req)=> {
 }
 
 const createJob = async(req, res)=> {
-    res.json(req.body)
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async(req, res)=> {
