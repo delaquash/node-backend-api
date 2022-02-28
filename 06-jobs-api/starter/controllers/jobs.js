a70df8c3b89cbedb953f9e55754177670084a7cf
delaquash <olaide1191@gmail.com>
2022-02-28 19:12:00 +0100

    implemented updateJob and getJob for job-api

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -8,7 +8,16 @@ const getAllJobs = async(req, res)=> {
 }
 
 const getJob = async(req, res)=> {
-    res.send('get single job')
+    // nested destructuring to pull out userId and jobId from req
+    const {user:{userId}, params:{id:jobId}} = req;
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId,
+    })
+    if(!job) {
+        throw new NotFoundError(`No job with the ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job });
 }
 
 const createJob = async(req, res)=> {
@@ -18,7 +27,27 @@ const createJob = async(req, res)=> {
 }
 
 const updateJob = async(req, res)=> {
-    res.send('update job')
+
+    const {
+        body: {company, position},
+        user:{ userId },
+        params: { id: jobId }
+    } = req;
+
+    if(company === '' || position === ''){
+        throw new BadRequestError('Company or Position cannot be empty')
+    }
+
+    const job = await Job.findByIdAndUpdate(
+        { _id: jobId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    )
+
+    if(!job) {
+        throw new NotFoundError(`No job with the ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job });
 }
 
 const deleteJob = async (req, res)=> {
