73f4498822a25d6af05d801d48e1c2f20f393d90
delaquash <olaide1191@gmail.com>
2022-03-17 22:42:37 +0100

    implemeted extra packages for jobs-api

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -5,6 +5,17 @@ const app = express();
 const connectDB = require( './db/connect.js');
 const authenticateUser = require('./middleware/authentication')
 
+
+// extra security package
+
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit');
+
+
+
+
 // router
 const authRouter = require('./routes/auth');
 const jobRouter = require('./routes/jobs');
@@ -15,6 +26,17 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(cors())
+app.use(helmet())
+app.use(xss())
+
 
 // routes
 app.use('/api/v1/auth', authRouter);
